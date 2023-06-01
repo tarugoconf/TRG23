a68b8aa4fd65a09127fb496b4275b052e16dfa79
Oscar Otero <oom@oscarotero.com>
2023-06-01 21:55:27 +0200

    avances

--- _config.ts
+++ _config.ts
@@ -1,8 +1,20 @@
 import lume from "lume/mod.ts";
 import date from "lume/plugins/date.ts";
+import postcss from "lume/plugins/postcss.ts";
 
 const site = lume()
   .use(date())
-  .ignore("README.md");
+  .use(postcss())
+  .ignore("README.md")
+  .copy("js")
+  .copy("img")
+  .remoteFile(
+    "_includes/css/normalize.css",
+    "https://unpkg.com/ui-normalize@0.3.0/ui-normalize.css",
+  )
+  .remoteFile(
+    "js/deps/vimeo-player.js",
+    "https://unpkg.com/@vimeo/player@2.20.1/dist/player.es.js",
+  );
 
 export default site;
