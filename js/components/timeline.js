bc2309a4e67cf4d6f13de9e91093f35b31ba42d1
Oscar Otero <oom@oscarotero.com>
2023-09-07 18:53:06 +0200

    timeline improvements

--- js/components/timeline.js
+++ js/components/timeline.js
@@ -4,15 +4,21 @@ class TRGTimeline extends HTMLElement {
   connectedCallback() {
     const data = this.dataset.events;
     const items = new DataSet(JSON.parse(data));
-
+    const start = new Date("2023-10-27 08:00");
+    const end = new Date("2023-10-27 08:00");
+    const hours = (window.innerWidth / 400); // 1 hour every 400px
+    end.setTime(start.getTime() + 1000 * 60 * 60 * hours);
     const options = {
       zoomable: false,
-      zoomMax: window.innerWidth > 1000
-        ? 60*60*3*1000
-        : 60*60*12*1000,
-      start: "2023-10-27 08:00",
-      minHeight: 460,
-      end: "2023-10-27 12:00",
+      start,
+      end,
+      height: 400,
+      groupHeightMode: "fixed",
+      orientation: {
+        axis: "bottom",
+        items: "top"
+      },
+      type: "range",
     };
 
     this.timeline = new Timeline(this, items, options);
