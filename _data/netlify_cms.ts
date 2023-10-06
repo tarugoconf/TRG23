fa24f35145403a84d1cdc350e44e73944c7ee2fd
Oscar Otero <oom@oscarotero.com>
2023-10-06 17:27:24 +0200

    bloque dirección

--- _data/netlify_cms.ts
+++ _data/netlify_cms.ts
@@ -23,104 +23,102 @@ const pages = f.files("Pages")
   .preview(false);
 
 pages.file("Portada", "index.yml", [
+  f.string("Title"),
+  f.object("About", [
     f.string("Title"),
-    f.object("About", [
+    f.markdown("Intro"),
+    f.object("Cta", [
+      f.string("Text"),
+      f.string("Url"),
+      f.markdown("Footer"),
+    ]),
+  ]),
+  f.object("Video", [
+    f.number("Id"),
+    f.image("Cover"),
+  ]),
+  f.object("Menu", [
+    f.list("Items", [
+      f.string("Text"),
+      f.string("Url"),
+    ]),
+  ]),
+  f.object("Block Speakers", [
+    f.string("Title"),
+    f.markdown("Intro"),
+  ]),
+  f.object("Block Calendar", [
+    f.string("Title"),
+    f.markdown("Intro"),
+    f.list("Days", [
       f.string("Title"),
-      f.markdown("Intro"),
-      f.object("Cta", [
-        f.string("Text"),
-        f.string("Url"),
-        f.markdown("Footer"),
-      ]),
-    ]),
-    f.object("Video", [
-      f.number("Id"),
-      f.image("Cover"),
-    ]),
-    f.object("Menu", [
       f.list("Items", [
-        f.string("Text"),
-        f.string("Url"),
-      ]),
-    ]),
-    f.object("Block Speakers", [
-      f.string("Title"),
-      f.markdown("Intro"),
-    ]),
-    f.object("Block Calendar", [
-      f.string("Title"),
-      f.markdown("Intro"),
-      f.list("Days", [
+        f.string("Time"),
         f.string("Title"),
-        f.list("Items", [
-          f.string("Time"),
-          f.string("Title"),
-          f.markdown("Description"),
-        ])
+        f.markdown("Description"),
       ]),
     ]),
-    f.object("Partners", [
-      f.string("Title"),
-      f.markdown("Intro"),
-      f.list("Items", [
-        f.image("Img").mediaFolder("img/partners"),
-        f.string("Url"),
-        f.string("Alt"),
-        f.string("Description"),
-      ]),
+  ]),
+  f.object("Partners", [
+    f.string("Title"),
+    f.markdown("Intro"),
+    f.list("Items", [
+      f.image("Img").mediaFolder("img/partners"),
+      f.string("Url"),
+      f.string("Alt"),
+      f.string("Description"),
     ]),
-    f.list("Collaborators", [
-      f.image("Img").mediaFolder("img/collaborators"),
+  ]),
+  f.list("Collaborators", [
+    f.image("Img").mediaFolder("img/collaborators"),
+    f.string("Url"),
+    f.string("Alt"),
+  ]),
+  f.object("Patrons", [
+    f.string("Title"),
+    f.markdown("Intro"),
+    f.list("Items", [
+      f.image("Img").mediaFolder("img/patrons"),
       f.string("Url"),
       f.string("Alt"),
     ]),
-    f.object("Patrons", [
-      f.string("Title"),
-      f.markdown("Intro"),
-      f.list("Items", [
-        f.image("Img").mediaFolder("img/patrons"),
-        f.string("Url"),
-        f.string("Alt"),
-      ]),
+  ]),
+  f.object("Tickets", [
+    f.string("Title"),
+    f.markdown("Intro"),
+    f.object("Cta", [
+      f.string("Text"),
+      f.string("Url"),
     ]),
-    f.object("Tickets", [
+  ]),
+  f.object("Community day", [
+    f.string("Title"),
+    f.markdown("Intro"),
+    f.object("Cta", [
+      f.string("Text"),
+      f.string("Url"),
+    ]),
+  ]),
+  f.object("Contact", [
+    f.string("Title"),
+    f.list("Items", [
       f.string("Title"),
-      f.markdown("Intro"),
-      f.object("Cta", [
+      f.string("Info"),
+      f.list("Links", [
         f.string("Text"),
         f.string("Url"),
+        f.image("Icon"),
       ]),
     ]),
-    f.object("Community day", [
-      f.string("Title"),
-      f.markdown("Intro"),
-      f.object("Cta", [
-        f.string("Text"),
-        f.string("Url"),
-      ]),
+  ]),
+  f.object("FAQ", [
+    f.string("Title"),
+    f.list("Questions", [
+      f.string("Question"),
+      f.markdown("Answer"),
     ]),
-    f.object("Contact", [
-      f.string("Title"),
-      f.list("Items", [
-        f.string("Title"),
-        f.string("Info"),
-        f.list("Links", [
-          f.string("Text"),
-          f.string("Url"),
-          f.image("Icon"),
-        ]),
-      ]),
-    ]),
-    f.object("FAQ", [
-      f.string("Title"),
-      f.list("Questions", [
-        f.string("Question"),
-        f.markdown("Answer"),
-      ]),
-    ])
-  ])
-
-
+  ]),
+]);
 
 // Create Data files collection
 const speakers = f.folder("Speakers", "speakers")
