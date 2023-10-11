05e5ad8fdf0d3ea8bce5aec62a864778b706f1a5
Oscar Otero <oom@oscarotero.com>
2023-10-11 21:08:47 +0200

    timeline sabado

--- js/components/timeline.js
+++ js/components/timeline.js
@@ -4,6 +4,7 @@ class TRGTimeline extends HTMLElement {
   connectedCallback() {
     const data = JSON.parse(this.dataset.events);
     const items = new DataSet(data);
+    const groups = this.dataset.groups ? JSON.parse(this.dataset.groups) : undefined;
     const first = new Date(data[0].start);
     const hours = window.innerWidth / 400;
 
@@ -12,6 +13,7 @@ class TRGTimeline extends HTMLElement {
     now.setDate(first.getDate());
     now.setMonth(first.getMonth());
     now.setFullYear(first.getFullYear());
+    now.setHours(15);
 
     const start = new Date(now);
     start.setTime(now.getTime() - 1000 * 60 * 60 * (hours / 2));
@@ -21,7 +23,7 @@ class TRGTimeline extends HTMLElement {
       zoomable: false,
       start,
       end,
-      height: 400,
+      minHeight: 400,
       groupHeightMode: "fixed",
       // order: (a, b) => a.start.getTime() - b.start.getTime(),
       orientation: {
@@ -36,7 +38,7 @@ class TRGTimeline extends HTMLElement {
       showMajorLabels: false,
     };
 
-    this.timeline = new Timeline(this, items, options);
+    this.timeline = new Timeline(this, items, groups, options);
     this.timeline.addCustomTime(now);
   }
 }
